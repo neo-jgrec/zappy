47bf25b636ed711e71f7b9a0c3ec02d1d5fe0d90
Zorg <raphael.mabille@epitech.eu>
2024-05-27 14:42:33 +0200

    feat(gui/basic): add basic connection encapsulation

--- src/gui/src/main.cpp
+++ src/gui/src/main.cpp
@@ -7,8 +7,23 @@
 
 #include <iostream>
 
-int main(int ac, char **av)
-{
-    std::cout << "Mais nan, c'est le GUI ouuu" << std::endl;
+#include "serverConnect.hpp"
+
+
+int main() {
+    try {
+        serverConnect server(3000, "127.0.0.1");
+        while (1) {
+            try {
+                std::string data = server.readFromServer();
+                std::cout << data << std::endl;
+            } catch (const std::exception& e) {
+                std::cerr << "Failed to read from the server: " << e.what() << std::endl;
+            }
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "Failed to connect to the server\n";
+        return 1;
+    }
     return 0;
 }
---
+++ src/gui/src/serverConnect.hpp
@@ -0,0 +1,68 @@
+/*
+** EPITECH PROJECT, 2024
+** zappy
+** File description:
+** serverConnect
+*/
+
+#ifndef INCLUDED_SERVERCONNECT_HPP
+    #define INCLUDED_SERVERCONNECT_HPP
+
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+    #include <string.h>
+    #include <iostream>
+    #include <vector>
+
+    class serverConnect
+    {
+        private:
+            int fd;
+
+        public:
+            std::string readFromServer();
+    
+            serverConnect(int port, const char *ip);
+            ~serverConnect();
+    };
+        
+    serverConnect::serverConnect(int port, const char *ip)
+    {
+        this->fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (this->fd < 0) {
+            throw std::runtime_error("Failed to create a socket");
+        }
+
+        struct sockaddr_in server_addr;
+        memset(&server_addr, 0, sizeof(server_addr));
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(port);
+
+        if (inet_pton(AF_INET, ip, &server_addr.sin_addr) <= 0) {
+            throw std::runtime_error("Invalid address");
+        }
+        if (connect(this->fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+            throw std::runtime_error("Failed to connect to the server");
+        }
+    }
+    
+    serverConnect::~serverConnect()
+    {
+        close(this->fd);
+    }
+
+    std::string serverConnect::readFromServer()
+    {
+        std::vector<char> buffer(2048);
+        ssize_t bytesRead = read(this->fd, buffer.data(), buffer.size() - 1);
+
+        if (bytesRead < 0) {
+            throw std::runtime_error("Failed to read from the server");
+        }
+        buffer[bytesRead] = '\0';
+        return std::string(buffer.data());
+    }
+
+#endif
