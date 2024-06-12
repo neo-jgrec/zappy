f758fb30073cbab141a1a148cb1a5a3400c03e11
Quentin <quentin.bollore@epitech.eu>
2024-06-12 16:45:57 +0200

    del(remove): survive from file

--- src/ai/run/patterns/Survive.cpp
+++ src/ai/run/patterns/Survive.cpp
@@ -10,51 +10,5 @@
 
 void Bot::survive()
 {
-    if (state.environment.tiles.empty())
-    {
-        queue.push_back({[&]()
-                         { doAction(LOOK, ""); }, "LOOK"});
-    }
-
-    std::unordered_map<std::string, std::function<void()>> actions = {
-        {"FORWARD", [&]()
-         { doAction(FORWARD, ""); }},
-        {"LEFT", [&]()
-         { doAction(LEFT, ""); }},
-        {"RIGHT", [&]()
-         { doAction(RIGHT, ""); }},
-        {"TAKE", [&]()
-         { doAction(TAKE, "food"); }}};
-
-    bool foodFound = false;
-
-    for (const auto &tile : state.environment.tiles)
-    {
-        // std::cout << "Tile: " << tile.x << " " << tile.y << std::endl;
-        // std::cout << "food: " << tile.ressources.food << std::endl;
-        // std::cout << "distance: " << tile.distance << std::endl;
-
-        if (tile.ressources.food > 0)
-        {
-            foodFound = true;
-            std::pair<int, int> coord = {tile.x, tile.y};
-            if (movementMap.find(coord) != movementMap.end())
-            {
-                for (const auto &move : movementMap[coord])
-                {
-                    queue.push_back({actions[move], move});
-                }
-            }
-            queue.push_back({actions["TAKE"], "TAKE"});
-            state.reward = state.ressources.food * 0.01;
-            break;
-        }
-    }
-
-    if (!foodFound)
-        queue.push_back({[&]()
-                         { doAction(FORWARD, ""); }, "FORWARD"});
-
-    queue.push_back({[&]()
-                     { doAction(LOOK, ""); }, "LOOK"});
+    searchAndTakeRessource("food");
 }
