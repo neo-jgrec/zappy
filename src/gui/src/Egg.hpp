7bc68187c425a2a2554b25f7bbdddae374265237
Zorg <raphael.mabille@epitech.eu>
2024-06-13 15:37:45 +0200

    feat(feat/gui/parsing): add getter for all eggs variables

--- src/gui/src/Egg.hpp
+++ src/gui/src/Egg.hpp
@@ -18,10 +18,15 @@ class Egg
         [[maybe_unused]] int playerNb;
         EggStatus state;
     public:
-        void setState(EggStatus newState) { state = newState; }
-
         Egg(std::vector<int> pos, int nb, int playerNb, EggStatus state): pos(pos), nb(nb), playerNb(playerNb), state(state) {};
         ~Egg() {};
+
+        void setState(EggStatus newState) { state = newState; }
+        
+        EggStatus getState() { return state; }
+        std::vector<int> getPos() { return pos; }
+        int getId() { return nb; }
+        int getPlayerNb() { return playerNb; }
 };
 
 
