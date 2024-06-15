92286e924f36a1912c68a817c46e5bb970f06dfd
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-06-15 23:56:26 +0200

    feat(server/commands/eject): add eject command

--- src/server/src/commands/ai/eject.c
+++ src/server/src/commands/ai/eject.c
@@ -7,7 +7,50 @@
 
 #include "server.h"
 
-void eject(client_t *client, server_t *server)
+static int reverse_direction(int direction)
 {
-    client_time_handler(client, EJECT);
+    switch (direction) {
+        case NORTH:
+            return SOUTH;
+        case SOUTH:
+            return NORTH;
+        case WEST:
+            return EAST;
+        case EAST:
+            return WEST;
+        default:
+            return -1;
+    }
+}
+
+void eject(client_t *c, server_t *s)
+{
+    unsigned char orientation = c->orientation;
+    signed char dx = 0;
+    signed char dy = 0;
+    client_list_t *node;
+    client_t *other_client;
+    signed char new_x;
+    signed char new_y;
+
+    if (orientation == NORTH)
+        dy = -1;
+    if (orientation == SOUTH)
+        dy = 1;
+    if (orientation == WEST)
+        dx = -1;
+    if (orientation == EAST)
+        dx = 1;
+    TAILQ_FOREACH(node, &s->clients, entries) {
+        other_client = node->client;
+        if (other_client->x == c->x && other_client->y == c->y && other_client != c) {
+            new_x = (c->x + dx + s->proprieties.width) % s->proprieties.width;
+            new_y = (c->y + dy + s->proprieties.height) % s->proprieties.height;
+            other_client->x = new_x;
+            other_client->y = new_y;
+            asprintf(&other_client->payload, "eject: %d\n", reverse_direction(orientation));
+        }
+    }
+    asprintf(&c->payload, "ok\n");
+    client_time_handler(c, EJECT);
 }
