036239f9663f5e1f2a1d23a006d2e17bcaa2ba8c
Quentin <quentin.bollore@epitech.eu>
2024-06-16 19:52:25 +0200

    feat(ia-utils): utils to change string

--- src/ai/src/utils/StringUtils.cpp
+++ src/ai/src/utils/StringUtils.cpp
@@ -20,3 +20,50 @@ std::string getElementAfter(const std::string& input, char delimiter)
 
     return "";
 }
+
+std::string getElementBefore(const std::string& input, char delimiter)
+{
+    std::istringstream iss(input);
+    std::string token;
+    
+    while (std::getline(iss, token, delimiter)) {
+        if (iss >> token) {
+            return input.substr(0, input.find(delimiter));
+        }
+    }
+
+    return "";
+}
+
+std::string generateNewNumber(const std::string &oldNumber, int offset)
+{
+    int num = std::stoi(oldNumber);
+    return std::to_string(num + offset);
+}
+
+std::string replaceNumbersInString(const std::string &input, int offset)
+{
+    std::regex numberRegex("\\d+");
+    std::string output = input;
+
+    std::sregex_iterator currentMatch(input.begin(), input.end(), numberRegex);
+    std::sregex_iterator lastMatch;
+
+    std::vector<std::pair<size_t, std::string>> replacements;
+
+    while (currentMatch != lastMatch) {
+        std::smatch match = *currentMatch;
+        std::string oldNumber = match.str();
+        std::string newNumber = generateNewNumber(oldNumber, offset);
+        replacements.push_back(
+            {match.position(), newNumber}
+        );
+        ++currentMatch;
+    }
+
+    for (auto it = replacements.rbegin(); it != replacements.rend(); ++it) {
+        output.replace(it->first, it->second.length(), it->second);
+    }
+
+    return output;
+}
