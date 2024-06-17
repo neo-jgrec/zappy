f838cb97b950664c7051d0abb47b38cbb2e17505
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-06-17 04:00:45 +0200

    feat(server/command/gui/pbc): add broadcast pbc event

--- src/server/src/commands/ai/broadcast.c
+++ src/server/src/commands/ai/broadcast.c
@@ -53,6 +53,19 @@ static int get_sound_direction(
     return 0;
 }
 
+static void send_broadcast_to_graphicals(client_t *client, server_t *server)
+{
+    client_list_t *client_list_entry;
+    client_t *receiver;
+
+    TAILQ_FOREACH(client_list_entry, &server->clients, entries) {
+        receiver = client_list_entry->client;
+        if (receiver->is_graphic)
+            dprintf(receiver->fd, "pbc %s %s\n",
+                client->uuid, client->commands[1]);
+    }
+}
+
 void broadcast(client_t *client, server_t *server)
 {
     client_list_t *client_list_entry;
@@ -73,5 +86,6 @@ void broadcast(client_t *client, server_t *server)
             direction, client->commands[1]);
     }
     asprintf(&client->payload, "ok\n");
+    send_broadcast_to_graphicals(client, server);
     client_time_handler(client, BROADCAST);
 }
