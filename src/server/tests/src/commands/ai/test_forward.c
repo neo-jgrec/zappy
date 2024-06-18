9f2834714154cd7763ae613dc2dbe0586a2c7601
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-06-18 05:05:32 +0200

    tests(server/forward): remove unecessary printf

--- src/server/tests/src/commands/ai/test_forward.c
+++ src/server/tests/src/commands/ai/test_forward.c
@@ -28,7 +28,6 @@ Test(forward, wrap_around) {
 
     forward(&client, &server);
 
-    printf("client.x, client.y: %d, %d\n", client.x, client.y);
     cr_assert_str_eq(client.payload, "ok\n", "Client payload not set correctly");
     cr_assert_eq(client.x, 5, "Client x-coordinate changed incorrectly");
     cr_assert_eq(client.y, 0, "Client y-coordinate not incremented as expected");
