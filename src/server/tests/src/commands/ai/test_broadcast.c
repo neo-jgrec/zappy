7bb1ca305a90816092c0484e231a71eff6d441f5
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-06-18 05:15:28 +0200

    tests(server/broadcast): add more tests

--- src/server/tests/src/commands/ai/test_broadcast.c
+++ src/server/tests/src/commands/ai/test_broadcast.c
@@ -38,3 +38,151 @@ Test(broadcast, message_format_test) {
     broadcast(&client, &server);
     cr_assert_str_eq(client2->client->payload, "message 2, Hello, world!\n");
 }
+
+Test(broadcast, from_east_to_west) {
+    server_t server;
+    server.proprieties.height = 10;
+    server.proprieties.width = 10;
+
+    client_t client;
+    client.x = 9;
+    client.y = 5;
+    client.level = 1;
+    client.commands = calloc(2, sizeof(char *));
+    client.commands[0] = strdup("Broadcast");
+    client.commands[1] = strdup("Hello, world!");
+
+    TAILQ_INIT(&server.clients);
+
+    srand(time(NULL));
+    for (int i = 0; i < 10; i++) {
+        client_t *new_client = malloc(sizeof(client_t));
+        new_client->x = 2;
+        new_client->y = 2;
+        new_client->level = 1;
+        client_list_t *new_client_list = malloc(sizeof(client_list_t));
+        new_client_list->client = new_client;
+        TAILQ_INSERT_TAIL(&server.clients, new_client_list, entries);
+    }
+
+    client_list_t *client2 = TAILQ_FIRST(&server.clients);
+
+    client_list_t *client_list_entry;
+    client_list_entry = malloc(sizeof(client_list_t));
+    client_list_entry->client = &client;
+    TAILQ_INSERT_TAIL(&server.clients, client_list_entry, entries);
+
+    broadcast(&client, &server);
+    cr_assert_str_eq(client2->client->payload, "message 4, Hello, world!\n");
+}
+
+Test(broadcast, from_west_to_east) {
+    server_t server;
+    server.proprieties.height = 10;
+    server.proprieties.width = 10;
+
+    client_t client;
+    client.x = 0;
+    client.y = 5;
+    client.level = 1;
+    client.commands = calloc(2, sizeof(char *));
+    client.commands[0] = strdup("Broadcast");
+    client.commands[1] = strdup("Hello, world!");
+
+    TAILQ_INIT(&server.clients);
+
+    srand(time(NULL));
+    for (int i = 0; i < 10; i++) {
+        client_t *new_client = malloc(sizeof(client_t));
+        new_client->x = 2;
+        new_client->y = 2;
+        new_client->level = 1;
+        client_list_t *new_client_list = malloc(sizeof(client_list_t));
+        new_client_list->client = new_client;
+        TAILQ_INSERT_TAIL(&server.clients, new_client_list, entries);
+    }
+
+    client_list_t *client2 = TAILQ_FIRST(&server.clients);
+
+    client_list_t *client_list_entry;
+    client_list_entry = malloc(sizeof(client_list_t));
+    client_list_entry->client = &client;
+    TAILQ_INSERT_TAIL(&server.clients, client_list_entry, entries);
+
+    broadcast(&client, &server);
+    cr_assert_str_eq(client2->client->payload, "message 4, Hello, world!\n");
+}
+
+Test(broadcast, from_north_to_south) {
+    server_t server;
+    server.proprieties.height = 10;
+    server.proprieties.width = 10;
+
+    client_t client;
+    client.x = 5;
+    client.y = 0;
+    client.level = 1;
+    client.commands = calloc(2, sizeof(char *));
+    client.commands[0] = strdup("Broadcast");
+    client.commands[1] = strdup("Hello, world!");
+
+    TAILQ_INIT(&server.clients);
+
+    srand(time(NULL));
+    for (int i = 0; i < 10; i++) {
+        client_t *new_client = malloc(sizeof(client_t));
+        new_client->x = 2;
+        new_client->y = 2;
+        new_client->level = 1;
+        client_list_t *new_client_list = malloc(sizeof(client_list_t));
+        new_client_list->client = new_client;
+        TAILQ_INSERT_TAIL(&server.clients, new_client_list, entries);
+    }
+
+    client_list_t *client2 = TAILQ_FIRST(&server.clients);
+
+    client_list_t *client_list_entry;
+    client_list_entry = malloc(sizeof(client_list_t));
+    client_list_entry->client = &client;
+    TAILQ_INSERT_TAIL(&server.clients, client_list_entry, entries);
+
+    broadcast(&client, &server);
+    cr_assert_str_eq(client2->client->payload, "message 8, Hello, world!\n");
+}
+
+Test(broadcast, from_south_to_north) {
+    server_t server;
+    server.proprieties.height = 10;
+    server.proprieties.width = 10;
+
+    client_t client;
+    client.x = 5;
+    client.y = 9;
+    client.level = 1;
+    client.commands = calloc(2, sizeof(char *));
+    client.commands[0] = strdup("Broadcast");
+    client.commands[1] = strdup("Hello, world!");
+
+    TAILQ_INIT(&server.clients);
+
+    srand(time(NULL));
+    for (int i = 0; i < 10; i++) {
+        client_t *new_client = malloc(sizeof(client_t));
+        new_client->x = 2;
+        new_client->y = 2;
+        new_client->level = 1;
+        client_list_t *new_client_list = malloc(sizeof(client_list_t));
+        new_client_list->client = new_client;
+        TAILQ_INSERT_TAIL(&server.clients, new_client_list, entries);
+    }
+
+    client_list_t *client2 = TAILQ_FIRST(&server.clients);
+
+    client_list_t *client_list_entry;
+    client_list_entry = malloc(sizeof(client_list_t));
+    client_list_entry->client = &client;
+    TAILQ_INSERT_TAIL(&server.clients, client_list_entry, entries);
+
+    broadcast(&client, &server);
+    cr_assert_str_eq(client2->client->payload, "message 8, Hello, world!\n");
+}
