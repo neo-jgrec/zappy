f0f403eb2935dd2647939e8cb7f890efea98e8dd
Quentin <quentin.bollore@epitech.eu>
2024-06-19 15:07:51 +0200

    feat(ai/group): group handling

--- src/ai/src/run/listeners/ListenBroadcastResponse.cpp
+++ src/ai/src/run/listeners/ListenBroadcastResponse.cpp
@@ -10,18 +10,17 @@
 void ABotProbabilistic::listenBroadcastResponse(const std::string &response)
 {
     std::string signature = getElementAfter(response, ':');
-    Message tempMessage;
     if (signature != _signature) {
         _enemyMessage = _message._content;
         std::cout << "Enemy message: " << _enemyMessage._content << std::endl;
         return;
     } else {
         std::string temp = getElementBefore(response, ':');
-        tempMessage._content = getElementAfter(temp, ',');
+        _allyMessage._content = getElementAfter(temp, ',');
         direction = getElementBefore(temp, ',');
         direction = getElementAfter(direction, ' ');
-        tempMessage.vigenereDecrypt();
-        printKeyValueColored("Message", tempMessage._content);
+        _allyMessage.vigenereDecrypt();
+        printKeyValueColored("Message", _allyMessage._content);
         printKeyValueColored("Direction", direction);
     }
 }
