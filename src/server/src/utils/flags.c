ee8a8a3bf96e6b49917895648636b1dc6e87bf20
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-06-19 17:30:08 +0200

    fix(server/flags): limit the map size to 1000

--- src/server/src/utils/flags.c
+++ src/server/src/utils/flags.c
@@ -141,9 +141,9 @@ static bool check_error_flags(flags_t *flags)
     if (flags->frequency < 2 || flags->frequency > 10000) {
         dprintf(2, "Frequency can only be between 2 and 10000\n");
         return false;
-    } else if (flags->width < 10 || flags->width > 30 ||
-            flags->height < 10 || flags->height > 30) {
-        dprintf(2, "Map values need to be between 10 and 30\n");
+    } else if (flags->width < 10 || flags->width > 1000 ||
+            flags->height < 10 || flags->height > 1000) {
+        dprintf(2, "Map values need to be between 10 and 1000\n");
         return false;
     }
     if (flags->nb_clients < 1 || flags->nb_clients > 200) {
