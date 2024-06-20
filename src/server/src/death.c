8c2323ae7942a6101c0003ae26c95362e25b118c
mohammed <molaryyios@gmail.com>
2024-06-20 02:52:52 +0200

    fix(server): segfault when client dies + handling win

--- src/server/src/death.c
+++ src/server/src/death.c
@@ -19,15 +19,14 @@ static bool handle_client_death(
 
     if (elapsed >= interval) {
         client->inventory.food -= 1;
-        printf("Client %d has %d food left\n", client->fd, client->inventory.food);
         clock_gettime(CLOCK_REALTIME, &client->live_time);
     }
     if (client->inventory.food == 0) {
         dprintf(client->fd, "dead\n");
         close(client->fd);
         FD_CLR(client->fd, &s->current_sockets);
-        remove_client_by_fd(&s->clients, client->fd);
         message_to_graphicals(s, "pdi %d\n", client->fd);
+        remove_client_by_fd(&s->clients, client->fd);
         return true;
     }
     return false;
