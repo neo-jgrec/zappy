b64ab72730727ccc3903e9ca29a3e0aad4d67485
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-06-22 21:25:56 +0200

    feat(client/gamblig): QrCode for link

--- client/src/pages/Gamble.tsx
+++ client/src/pages/Gamble.tsx
@@ -1,12 +1,18 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { DonutChart, LineChart, ScaleTypes } from "@carbon/charts-react";
 import { Layout } from "../components";
 import { useTheme, useWebSocket } from "../context";
+import QRCode from 'qrcode.react';
 import '@carbon/charts/styles.css'
+import { Button, Modal } from "@carbon/react";
+import {
+  QrCode
+} from '@carbon/icons-react';
 
 function Gamble() {
   const { teamScoresState } = useWebSocket();
   const { theme } = useTheme();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const transformData = useMemo(() => {
     const data: { group: string; date: Date; value: any; }[] = [];
@@ -67,7 +73,15 @@ function Gamble() {
 
   return (
     <Layout>
-      <h1>Gambling</h1>
+      <div className='flex justify-between mb-10'>
+        <h1>Gambling</h1>
+        <Button
+          onClick={() => setIsModalOpen(true)}
+          renderIcon={QrCode}
+        >
+          Scan to join
+        </Button>
+      </div>
       <div className='w-full h-[400px] flex items-center justify-center flex-row'>
         <div className='w-2/3 h-full'>
           <LineChart
@@ -83,6 +97,20 @@ function Gamble() {
           />
         </div>
       </div>
+      <Modal
+        open={isModalOpen}
+        modalHeading='Scan to join the game'
+        primaryButtonText='Close'
+        onRequestClose={() => setIsModalOpen(false)}
+        onRequestSubmit={() => setIsModalOpen(false)}
+        onSecondarySubmit={() => setIsModalOpen(false)}
+      >
+        <QRCode
+          className='w-1/2 mx-auto'
+          size={500}
+          value={window.location.href}
+        />
+      </Modal>
     </Layout>
   );
 }
