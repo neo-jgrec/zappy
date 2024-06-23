38c71eb517b028668343f42a6d399cc07658bf08
Quentin <quentin.bollore@epitech.eu>
2024-06-23 02:56:01 +0200

    feat(ia-shared): new state for evoBot

--- src/ai/src/bots/Forker.hpp
+++ src/ai/src/bots/Forker.hpp
@@ -9,18 +9,33 @@
 #define FORKER_HPP_
 
 #include "ABotPattern.hpp"
+#include "../constant/Constants.hpp"
 #include <sys/wait.h>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <map>
+#include <limits>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
 
 class Forker : public ABotPattern
 {
 public:
     void initChild() override;
     void updateStrategy() override;
+    Ressources parseRessources(const std::string& ressourcesString);
+    std::pair<bool, NeededResources> canLevelUp(int currentLevel);
+    std::vector<int> identifyEfficientBots(int currentLevel, const NeededResources& neededResources);
 
 private:
     unsigned int _idBot = 1;
 
     void forkNewBot();
+    std::map<int, Ressources> sharedInventory;
+    BotState _evoBot;
 };
 
 #endif // FORKER_HPP_
