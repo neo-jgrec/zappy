1ffc2a4284c97c7ebd3e4092cc83417a60fca155
Quentin <quentin.bollore@epitech.eu>
2024-06-23 17:33:47 +0200

    feat(ia-shared: new shared handling include

--- src/ai/src/bots/Forker.hpp
+++ src/ai/src/bots/Forker.hpp
@@ -28,13 +28,17 @@ public:
     void updateStrategy() override;
     Ressources parseRessources(const std::string& ressourcesString);
     std::pair<bool, NeededResources> canLevelUp(int currentLevel);
-    std::vector<int> identifyEfficientBots(int currentLevel, const NeededResources& neededResources);
+    std::map<int, Ressources> identifyEfficientBots(int currentLevel, NeededResources& neededResources);
+    std::pair<NeededResources, std::map<int, Ressources>> calculateAndPrintBotDeposits(std::map<int, Ressources>& sharedInventory, int currentLevel, NeededResources& neededResources);
+    std::map<int, Ressources> combineInventories(const std::map<int, Ressources>& inventory1, const std::map<int, Ressources>& inventory2);
+    std::string formatBotContributions(const std::map<int, Ressources>& botContributions, const std::map<int, Ressources>& botContributions2);
 
 private:
     unsigned int _idBot = 1;
 
     void forkNewBot();
     std::map<int, Ressources> sharedInventory;
+    std::map<int, Ressources> evoBotInventory;
     BotState _evoBot;
 };
 
